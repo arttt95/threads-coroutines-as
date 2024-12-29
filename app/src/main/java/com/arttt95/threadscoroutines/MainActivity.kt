261cddbfffd6ad39379aa02e51aadb4bd7592778
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-29 16:48:49 -0300

    29DEZ24 - Aula 211 AS - Criando Threads.

--- app/src/main/java/com/arttt95/threadscoroutines/MainActivity.kt
+++ app/src/main/java/com/arttt95/threadscoroutines/MainActivity.kt
@@ -2,11 +2,14 @@ package com.arttt95.threadscoroutines
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.arttt95.threadscoroutines.databinding.ActivityMainBinding
+import java.lang.Thread.currentThread
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +34,30 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.btnIniciar.setOnClickListener {
+
+//            MinhaThread().start()
+            repeat(15) { indice ->
+                Log.i("info_thread", "Executando $indice -> T: ${currentThread().name}")
+                sleep(1000)
+            }
+
+        }
+
     }
+
+    inner class MinhaThread : Thread() {
+
+        override fun run() {
+            super.run()
+
+            repeat(30) { indice ->
+                Log.i("info_thread", "Executando $indice -> T: ${currentThread().name}")
+                sleep(1000)
+            }
+
+        }
+
+    }
+
 }
